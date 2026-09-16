7dd2d098b0fd4995ad83bc6149f1cd67e3ad6360
routersys <github.routersys@gmail.com>
2026-09-16 09:08:30 +0900

    パラメータの読み込みでアニメーションの値が失われるのを直す

--- StringSpectrum/StringSpectrumParameter.cs
+++ StringSpectrum/StringSpectrumParameter.cs
@@ -17,6 +17,10 @@ namespace StringSpectrum
             StringSpectrumUpdateNotifier.EnsureCheckedOnce();
         }
 
+        public StringSpectrumParameter() : this(null)
+        {
+        }
+
         [Display(Name = nameof(Texts.StringWidth), Description = nameof(Texts.StringWidthDescription), Order = 10, ResourceType = typeof(Texts))]
         [AnimationSlider("F1", "px", 0, 1920)]
         public Animation StringWidth { get; } = new Animation(600, 0.01, YMM4Constants.VeryLargeValue);
